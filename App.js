84f68d4e8b5f6b0b3b8fd482892f73a36be8b252
KAVINDYADEVINDI <kavindyadewindi12345678@gmail.com>
2021-06-11 11:34:47 -0700

    second commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>kavindya devindi!</Text>
+      <Text>kavindya devindi hello!</Text>
       <StatusBar style="auto" />
     </View>
   );
